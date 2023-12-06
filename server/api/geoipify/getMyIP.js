9748b2c1d43fb806568109759cac16fca1fd9dfe
rizkiismail <laksmimentari@gmail.com>
2023-12-06 11:47:26 +0700

    update

--- server/api/geoipify/getMyIP.js
+++ server/api/geoipify/getMyIP.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default defineEventHandler(async () => {
   const runTimeConfig = useRuntimeConfig();
   // const data = await $fetch(`https://api.ipify.org`);
-  const { data: myIP } = await axios.get(`https://api.ipify.org`);
-  const { data: result } = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=${runTimeConfig.public.GEOIPIFY_KEY}&ipAddress=${myIP}`);
+  // const { data: myIP } = await axios.get(`https://api.ipify.org`);
+  const { data: result } = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=${runTimeConfig.public.GEOIPIFY_KEY}`);
   return result;
 });
