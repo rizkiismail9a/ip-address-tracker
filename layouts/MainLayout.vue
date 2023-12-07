aaf03cfeb3ceed465b19381ad41efcbfab57fc79
rizkiismail <laksmimentari@gmail.com>
2023-12-07 07:04:50 +0700

    fixing UI bug

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="MainLayout" class="fixed w-full h-screen">
     <div class="headerImage">
-      <img :src="imgSrc" class="object-cover w-full" alt="" />
+      <img src="/images/pattern-bg-desktop.png" alt="" class="object-cover w-full isPC md:block hidden" />
+      <img src="/images/pattern-bg-mobile.png" alt="" class="object-cover w-full md:hidden block" />
     </div>
 
     <slot></slot>
@@ -14,26 +15,6 @@
 
 <script setup>
 import MapView from "~/components/MapView.vue";
-const imgSrc = ref("");
-let windowWidth = ref(process.client ? window.innerWidth : "");
 import { useLocationStore } from "../stores/location";
 const locationStore = useLocationStore();
-onBeforeMount(() => {
-  window.addEventListener("resize", () => {
-    windowWidth.value = window.innerWidth;
-  });
-});
-watch(
-  () => windowWidth.value,
-  () => {
-    if (windowWidth.value < 768) {
-      imgSrc.value = "/images/pattern-bg-mobile.png";
-    } else {
-      imgSrc.value = "/images/pattern-bg-desktop.png";
-    }
-  },
-  {
-    immediate: true,
-  }
-);
 </script>
